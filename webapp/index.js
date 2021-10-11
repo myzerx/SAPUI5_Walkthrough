fdfe8f63385c847d52c6a24a036aec91a78c3dd4
Richard <eternal.cassola@gmail.com>
2021-10-11 10:48:54 -0300

    Step 10 - Descriptor for Applications

--- webapp/index.js
+++ webapp/index.js
@@ -1,13 +1,14 @@
 sap.ui.define([
-	"sap/ui/core/ComponentContainer"
+    "sap/ui/core/ComponentContainer"
 ], function (ComponentContainer) {
 	"use strict";
 
 	new ComponentContainer({
 		name: "sap.ui.demo.walkthrough",
 		settings : {
-			id : "walkthrough"
+			id: "walkthrough"
 		},
 		async: true
 	}).placeAt("content");
+
 });
